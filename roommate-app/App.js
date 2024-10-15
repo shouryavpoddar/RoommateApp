d4fbe7d5d2618ce75fc6439781f1847916c55a8d
Ethan Tobey <etobey04@gmail.com>
2024-10-14 22:13:55 -0400

    Moved app.js out of /src to resolve dependency issues with auto generated node_modules

--- roommate-app/App.js
+++ roommate-app/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className='flex-1 justify-center items-center bg-gray-300'>
+    <View className='flex-1 justify-center items-center bg-blue-300'>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
--- roommate-app/tailwind.config.js
+++ roommate-app/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
