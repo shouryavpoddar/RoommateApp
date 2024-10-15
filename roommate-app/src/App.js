9ca1556f0009db751f15ab4563316bd0435d9d62
SreyaSrinidhi <sks190@case.edu>
2024-10-14 20:47:57 -0400

    added tailwind.css

--- roommate-app/src/App.js
+++ roommate-app/src/App.js
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className='flex-1 justify-center items-center bg-gray-300'>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
