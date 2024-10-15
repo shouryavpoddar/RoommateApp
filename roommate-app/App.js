92913be3f4531d21b839ba98c3dfa3780ba58d79
Ethan Tobey <etobey04@gmail.com>
2024-10-14 22:36:11 -0400

    home page format files

--- roommate-app/App.js
+++ roommate-app/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import HomePage from './src/HomePage/HomePage';
 
 export default function App() {
   return (
     <View className='flex-1 justify-center items-center bg-blue-300'>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <HomePage></HomePage>
     </View>
   );
 }
