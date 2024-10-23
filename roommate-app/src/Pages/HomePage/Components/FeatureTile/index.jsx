691cd5721fdeed485c178e5fab17a75a39f35114
Ethan Tobey <emt101@case.edu>
2024-10-22 21:40:14 -0700

    homeWidget integration to home page

---
+++ roommate-app/src/Pages/HomePage/Components/FeatureTile/index.jsx
@@ -0,0 +1,13 @@
+import { Text, TouchableOpacity } from 'react-native';
+
+const onPress = () => {console.log("you clicked me!")};
+
+export default function FeatureTile({ bgColor, flex, text, TileComponent, onPress}) {
+    return (
+        <TouchableOpacity 
+            className={`rounded-md flex justify-center items-center mx-1.5 ${bgColor} ${flex} ${TileComponent ? '' : 'h-48'}`} 
+            onPress={onPress}>
+            {TileComponent ? <TileComponent /> : <Text className="text-white text-xl font-bold">{text}</Text>}
+        </TouchableOpacity>    
+    )
+}
