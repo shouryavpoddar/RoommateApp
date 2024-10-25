96f662837c5e5287701726b847ab822b00bfd781
Ethan Tobey <emt101@case.edu>
2024-10-24 18:29:40 -0700

    set up React Native Testing Library, installed necessary dependencies and modified config files. Made first basic test

--- roommate-app/babel.config.js
+++ roommate-app/babel.config.js
@@ -1,7 +1,18 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel", 'react-native-reanimated/plugin'],
+    presets: [
+      'babel-preset-expo',
+      '@babel/preset-typescript', // Add TypeScript preset
+    ],
+    plugins: [
+      "nativewind/babel",
+      'react-native-reanimated/plugin',
+    ],
+    env: {
+      test: {
+        plugins: ['@babel/plugin-transform-runtime'],
+      },
+    },
   };
 };
---
+++ roommate-app/jest.config.js
@@ -0,0 +1,12 @@
+  module.exports = {
+    preset: 'react-native', // Use the React Native preset
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Transform JS, JSX, TS, and TSX using Babel
+    },
+    transformIgnorePatterns: [
+      'node_modules/(?!(react-native|react-navigation|react-native-calendars|react-native-swipe-gestures|react-native-vector-icons|@react-native|@react-navigation|react-redux|@react-native-vector-icons|expo|expo-status-bar|expo-modules-core|nativewind)/)', 
+      // Add necessary packages here to be transformed
+    ],
+    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'], // Setup custom matchers
+    testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'], // Ignore these paths for tests
+};
